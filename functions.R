b2d23a2d95126ea314fd0dbe823d980f833872e6
Davit Kartashyan <davsmile@yahoo.com>
2013-09-27 06:55:52 +0800

    R functions

---
+++ functions.R
@@ -0,0 +1,19 @@
+f.upd3<-function(data,y,folds=10){  
+  selected <- list()
+  score_hist <- list()
+  while(length(score_hist) < 2 || last(score_hist) < last(score_hist,1)){
+    if(length(selected) > 0){
+      newdata<-lapply(data,cbind,selected,y=y)
+    }
+    else{
+      newdata<-lapply(data,cbind,y=y)      
+    }
+    res<-lapply(newdata,cv3,folds)
+    min.index <- which.min(res)
+    score_hist<-c(score_hist,res[[min.index]])
+    cat(names(min.index),res[[min.index]],'\n')
+    selected <- c(selected,subset(data,select=min.index))
+    data <- data[,-min.index]
+  }
+  return(list(selected=head(selected,-1),score_hist=score_hist))
+}
