3e9a4de003da63ae18d9d450192667c3f0613dc8
Davit Kartashyan <davsmile@yahoo.com>
2013-09-27 16:52:03 +0800

    backward elimination with R step()

--- functions.R
+++ functions.R
@@ -1,19 +1,53 @@
-backward.selection<-function(data,y,folds=9){  
-  selected <- data
-  score_hist <- list()
-  while(length(score_hist) < 2 || last(score_hist) < last(score_hist,1)){
-    if(length(selected) > 0){
-      newdata<-lapply(data,cbind,selected,y=y)
-    }
-    else{
-      newdata<-lapply(data,cbind,y=y)      
-    }
-    res<-lapply(newdata,cv3,folds)
-    min.index <- which.min(res)
-    score_hist<-c(score_hist,res[[min.index]])
-    cat(names(min.index),res[[min.index]],'\n')
-    selected <- c(selected,subset(data,select=min.index))
-    data <- data[,-min.index]
-  }
-  return(list(selected=head(selected,-1),score_hist=score_hist))
+backward.selection<-function(data,y,folds=10){  
+  model <- lm(y ~ ., data = cbind(data,y))
+  back.elim <- step(model,direction="backward",trace=F)
+  return(back.elim)
+}
+
+get.poly<-function(dat,degree){
+  poly<-data.frame(lapply(dat,poly,data=degree,raw=T))
+}
+
+lm_prediction <- function(lm.f,test){
+  names(test)<-c('apcp_sfc','dlwrf_sfc','dswrf_sfc','pres_msl','pwat_eatm','spfh_2m','tcdc_eatm','tcolc_eatm','tmax_2m','tmin_2m', 'tmp_2m','tmp_sfc','ulwrf_sfc','ulwrf_tatm','uswrf_sf') 
+  sc.test<-data.frame(scale(test))
+  testpoly3<-get.poly(sc.test,3)
+  pred<-predict(lm.f,testpoly3)
+  submat<-data.frame(matrix(pred,ncol=98,byrow=T))
+  return(submat)
+}
+
+get.station_names<-function(f){
+  st.names<-read.csv(f,nrow=1) 
+  st.names<-names(st.names)[-1]
+}
+
+readData<-function(fname='spline_train.csv'){
+  data = read.csv(fname,header=F)
+  dat = data[,c(2:16)]
+  target = data[,17]
+  names(dat)<-c('apcp_sfc','dlwrf_sfc','dswrf_sfc','pres_msl','pwat_eatm','spfh_2m','tcdc_eatm','tcolc_eatm','tmax_2m','tmin_2m', 'tmp_2m','tmp_sfc','ulwrf_sfc','ulwrf_tatm','uswrf_sf')
+  return(list(dat = dat, target = target))
+}
+
+conclude<-function(datList){
+  dat<-datList$dat
+  target<-data.frame(y=datList$target)
+  scdat<-data.frame(scale(dat))
+  poly3<-get.poly(scdat,3)
+  model<-backward.selection(poly3,target)
+}
+#test<-read.csv("C:/Users/Administrator/Documents/Java_Proj/eclipse/python/solar/src/sol/spline_test.csv",header=F)
+test.model<-function(test,model){
+  sub<-lm_prediction(model,test)
+  return(sub)
+}
+
+#write.submission("E:/kaggle/Solar/gefs_test/test/sampleSubmission.csv","C:/Users/Administrator/Documents/Java_Proj/eclipse/python/solar/src/sol/back.elim.submission.csv",tm)
+write.submission<-function(infn,outfn,submat){
+  samplefile<-read.csv(infn)
+  sf<-samplefile[,1]
+  names(submat)<-get.station_names(infn)
+  sub<-cbind(Date=sf,submat)
+  write.csv(sub,outfn,row.names=F)
 }
